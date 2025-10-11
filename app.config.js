ec935a768808dd15a9bb8fcc93ae16328d307fbd
Salami Abdulmalik Adewale <salami.adewale@iuokada.edu.ng>
2025-10-11 15:00:11 +0000

    Fix app.config.js to preserve EAS projectId

--- app.config.js
+++ app.config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = ({ config }) => ({
   ...config,
   extra: {
+    ...config.extra,
     mapTilerKey: process.env.EXPO_PUBLIC_MAPTILER_KEY,
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
